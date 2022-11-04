bb03cf0500426e74c49404c5c5b9276eea0cc7d3
Anthony G <agithaka@live.com>
2022-11-05 01:01:44 +0300

    refactor: nect config - add env vars

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: { appDir: true },
+  env: {
+    SUPA_URL: process.env.SUPA_URL,
+    SUPA_PWD: process.env.SUPA_PWD,
+    SUPA_KEY: process.env.SUPA_KEY,
+  }
 }
 
 module.exports = nextConfig
